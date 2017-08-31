d431576f7dc0c124690fc561c5d6636da3cf4614
Sidhra <sidhra.cader@hotmail.com>
2017-08-31 16:13:40 +1000

    completed more-functions exercise

--- 01-javascript/functions/more-functions.js
+++ 01-javascript/functions/more-functions.js
@@ -1,9 +1,15 @@
 //QUESTION_ONE
+var newArray;
+
 var lengths = function(stringArray){
-  var newArray = [];
-  for (var i = 0; i < stringArray.length; i++) {
-    newArray.push(stringArray[i].length);
+  newArray = [];
+  // for (var i = 0; i < stringArray.length; i++) {
+  //   newArray.push(stringArray[i].length);
+  // }
+  var wordLength = function(element){
+    newArray.push(element.length);
   }
+  stringArray.forEach(wordLength);
   return newArray;
 }
 
@@ -20,8 +26,22 @@ console.log(transmogrifier(2, 2, 3));
 
 //QUESTION_THREE
 var wordReverse = function(word){
-  var reverseWord = word.split("").reverse().join().replace(/,/g, "");
+  var reverseWord = word.split(" ").reverse().join(" ");
   return reverseWord;
 }
 
-console.log(wordReverse("hello world"));
+console.log(wordReverse("Banana is yellow"));
+
+//FOR EACH EXAMPLE
+// var numbers = [1, 2, 3];
+// var ansArr = [];
+//
+// var myForEach = function(arr, fn){
+//   fn(arr[0])
+// }
+//
+// var doubleAndLog = function(num){
+//   console.log(num * 2);
+// }
+//
+// console.log(myForEach(numbers, doubleAndLog));
